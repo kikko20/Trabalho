d0bbf2c2bdf369b2753642ef683b1b2f7ce52cf3
Italo Lima Ferreira <ferreiraitalo180@gmail.com>
2023-09-18 11:54:25 -0300

    Anime

---
+++ PROJETO/Assets/Scripts/Inimigo1.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.UIElements;
+using UnityEngine;
+
+public class Inimigo1 : MonoBehaviour
+{
+    public int Speed;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
