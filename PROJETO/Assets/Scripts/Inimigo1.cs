fc2461a3adf293a17cfcdb23354ebf45301c5171
Italo Lima Ferreira <ferreiraitalo180@gmail.com>
2023-09-25 12:01:03 -0300

    sla

--- PROJETO/Assets/Scripts/Inimigo1.cs
+++ PROJETO/Assets/Scripts/Inimigo1.cs
@@ -6,16 +6,32 @@ using UnityEngine;
 public class Inimigo1 : MonoBehaviour
 {
     public int Speed;
-    
+
+    private Rigidbody2D rig;
+    private bool movingRight = true;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rig = GetComponent<Rigidbody2D>();
+
+        // Inicia o movimento do inimigo em uma nova thread
+        InvokeRepeating("Move", 0f, 5f);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Move()
     {
-        
+        if (movingRight)
+        {
+            rig.velocity = new Vector2(Speed, rig.velocity.y);
+        }
+        else
+        {
+            rig.velocity = new Vector2(-Speed, rig.velocity.y);
+        }
+
+        movingRight = !movingRight;
     }
 }
+
+
