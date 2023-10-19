29becf64811c59228f9dfca86b120f5ab3417ad2
Suelton Felipe Alves Pereira <suelton.a@academico.ifrn.edu.br>
2023-10-19 11:54:28 -0300

    trabalho

--- PROJETO/Assets/script/AtirarBossScript.cs
+++ PROJETO/Assets/script/AtirarBossScript.cs
@@ -6,27 +6,19 @@ public class AtirarBossScript : MonoBehaviour
     public Transform pontoDeSpawnLaiser; // Objeto vazio que representa o ponto de spawn
     public float tempoEntreTiros = 2.0f; // Tempo em segundos entre cada tiro
     private float tempoUltimoTiro = 0.0f;
-    private Animator animator; // Referência ao componente Animator
     private BossScript bossScript; // Referência ao script principal do boss
-    public int vidaInicial = 10; // Variável para a vida inicial do boss
 
     void Start()
     {
-        animator = GetComponent<Animator>();
         bossScript = GetComponent<BossScript>();
     }
 
     void Update()
     {
-        // Verifique se o boss tem 50% ou menos de vida para ativar a animação "atirando"
-        if (bossScript.vida <= vidaInicial / 2)
+        // Verifique se o boss tem mais de 50% de vida para atirar
+        if (bossScript.vida > bossScript.vidaInicial / 2)
         {
             AtirarLaiser();
-            animator.SetBool("Atirando", true); // Ativar a animação "atirando"
-        }
-        else
-        {
-            animator.SetBool("Atirando", false); // Desativar a animação "atirando"
         }
     }
 
